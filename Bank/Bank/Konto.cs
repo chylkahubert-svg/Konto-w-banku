3b77eff9d830c76b2852b07fa49820e11b62a139
Hubert Chylka <chylkahubert@gmail.com>
2026-03-25 18:04:18 +0100

    kod dla klasy Konto i stworzenie klas KontoLimit i KontoPlus

--- Bank/Bank/Konto.cs
+++ Bank/Bank/Konto.cs
@@ -6,5 +6,65 @@ namespace Bank
 {
     internal class Konto
     {
+        private string klient;              //nazwa klienta
+        private decimal bilans;             // aktualny stan środków
+        private bool zablokowane = false;   // status konta
+
+        //Konstruktor domyślny byłby zbędny - nie chcemy kont bez nazwy klienta
+
+        public Konto(string klient, decimal bilansNaStart = 0)
+        {
+            if (string.IsNullOrWhiteSpace(klient))
+                throw new ArgumentException("Nazwa klienta nie może być pusta.");
+
+            if (bilansNaStart < 0)
+                throw new ArgumentException("Bilans początkowy nie może być ujemny.");
+
+            this.klient = klient;
+            this.bilans = bilansNaStart;
+        }
+
+        //Właściwości tylko do odczytu
+        public string Nazwa => klient;
+        public decimal Bilans => bilans;
+        public bool Zablokowane => zablokowane;
+
+        //Blokowanie i odblokowywanie konta
+        public void BlokujKonto()
+        {
+            zablokowane = true;
+        }
+
+        public void OdblokujKonto()
+        {
+            zablokowane = false;
+        }
+
+        //Metoda wpłaty
+        public void Wplata(decimal kwota)
+        {
+            if (zablokowane)
+                throw new InvalidOperationException("Konto jest zablokowane.");
+
+            if (kwota <= 0)
+                throw new ArgumentException("Kwota wpłaty musi być dodatnia.");
+
+            bilans += kwota;
+        }
+
+        // Metoda wypłaty
+        public void Wyplata(decimal kwota)
+        {
+            if (zablokowane)
+                throw new InvalidOperationException("Konto jest zablokowane.");
+
+            if (kwota <= 0)
+                throw new ArgumentException("Kwota wpłaty musi być dodatnia.");
+
+            if (kwota > bilans)
+                throw new InvalidOperationException("Brak wystarczających środków na koncie.");
+
+            bilans -= kwota;
+        }
     }
 }
--- Bank/Bank/KontoPlus.cs
+++ Bank/Bank/KontoPlus.cs
@@ -1,6 +1,6 @@
 ﻿namespace Bank
 {
-    public class Class1
+    public class KontoPlus
     {
 
     }
