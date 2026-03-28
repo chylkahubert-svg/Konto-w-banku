a700c008793a2d4e7790671bce1e5c1dc818c01c
Hubert Chylka <chylkahubert@gmail.com>
2026-03-28 14:59:55 +0100

    Reszta testów dla klasy Konto

--- Bank/TestKonto/Test1.cs
+++ Bank/TestKonto/Test1.cs
@@ -36,5 +36,71 @@ namespace TestKonto
 
             Assert.AreEqual(150m, konto.Bilans);
         }
+
+        [TestMethod]
+        public void Wplata_NieprawidlowaKwota_Throw()
+        {
+            var konto = new Konto("Hubert", 100);
+
+            konto.Wplata(0);
+        }
+
+        [TestMethod]
+        public void Wyplata_PrawidlowaKwota_ZmniejszaBilans()
+        {
+            var konto = new Konto("Hubert", 100);
+
+            konto.Wyplata(40);
+
+            Assert.AreEqual(60m, konto.Bilans);
+        }
+
+        [TestMethod]
+        public void Wyplata_ZaDuzo_Throw()
+        {
+            var konto = new Konto("Hubert", 100);
+
+            konto.Wyplata(150);
+        }
+
+        [TestMethod]
+        public void BlokujKonto_UstawieniaZablokowaneNaTrue()
+        {
+            var konto = new Konto("Hubert", 100);
+            konto.BlokujKonto();
+
+            konto.OdblokujKonto();
+
+            Assert.IsFalse(konto.Zablokowane);
+        }
+
+        [TestMethod]
+        public void OdblokujKonto_UstawieniaZablokowaneFalse()
+        {
+            var konto = new Konto("Hubert", 100);
+            konto.BlokujKonto();
+
+            konto.OdblokujKonto();
+
+            Assert.IsFalse(konto.Zablokowane);
+        }
+
+        [TestMethod]
+        public void Wplata_GdyZablokowane_Throw()
+        {
+            var konto = new Konto("Hubert", 100);
+            konto.BlokujKonto();
+
+            konto.Wplata(50);
+        }
+
+        [TestMethod]
+        public void Wyplata_GdyZablokowane_Throw()
+        {
+            var konto = new Konto("Hubert", 100);
+            konto.BlokujKonto();
+
+            konto.Wyplata(50);
+        }
     }
 }
