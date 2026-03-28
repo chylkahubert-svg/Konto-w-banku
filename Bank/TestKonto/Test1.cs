a07e9b604e503f15af7c7681542c1f276fff7b18
Hubert Chylka <chylkahubert@gmail.com>
2026-03-28 14:18:46 +0100

    Dodanie plików testowych dla Konto, KontoPlus i KontoLimit oraz pierwsza część testów w TestKonto

---
+++ Bank/TestKonto/Test1.cs
@@ -0,0 +1,40 @@
+﻿using Bank;
+
+namespace TestKonto
+{
+    [TestClass]
+    public class Test1
+    {
+        [TestMethod]
+        public void Konstruktor_PoprawneDane_TworzyKonto()
+        {
+            var konto = new Konto("Hubert", 100);
+
+            Assert.AreEqual("Hubert", konto.Nazwa);
+            Assert.AreEqual(100m, konto.Bilans);
+            Assert.IsFalse(konto.Zablokowane);
+        }
+
+        [TestMethod]
+        public void Konstruktor_PustaNazwa_Throw()
+        {
+            var konto = new Konto("", 0);
+        }
+
+        [TestMethod]
+        public void Konstruktor_UjemnyBilans_Throw()
+        {
+            var konto = new Konto("Hubert", -10);
+        }
+
+        [TestMethod]
+        public void Wplata_PrawidlowaKwota_ZwiekszaBilans()
+        {
+            var konto = new Konto("Hubert", 100);
+
+            konto.Wplata(50);
+
+            Assert.AreEqual(150m, konto.Bilans);
+        }
+    }
+}
---
+++ Bank/TestKontoLimit/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace TestKontoLimit
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
